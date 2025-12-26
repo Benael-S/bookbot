d00aa0851ba4b82eca0d22f838b8f69360bf08c5
Benael-S <benael.saada@gmail.com>
2025-12-26 15:49:53 +0200

    changed main.py to use reletive path instead of absolute path

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from stats import get_num_words, cnt_char#import functions from stats.py 
 
+path = "books/frankenstein.txt" #book path
+
 def get_book_text(path_to_file):
     with open(path_to_file, "r") as f:
         return f.read()
 # takes text file path and returns its contents
 
 def main():
-    path = "/home/benael/projects/github.com/bookbot/books/frankenstein.txt"
     text = get_book_text(path)
     return text 
 #calls the prev function and input the path, return total file contents
@@ -17,3 +18,6 @@ book_txt = str(result_m) # convert results to string
 
 get_num_words(book_txt)#start the whole prog
 cnt_char(book_txt)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path}...")
