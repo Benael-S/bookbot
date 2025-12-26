3f1cb2c58b03e5d63b169e2014593027ad88aadc
Benael-S <benael.saada@gmail.com>
2025-12-26 18:07:31 +0200

    finalized main+stats code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from stats import get_num_words, cnt_char#import functions from stats.py 
+from stats import get_num_words, cnt_char, dic_to_list
+#import functions from stats.py 
 
-path = "books/frankenstein.txt" #book path
+path = "books/frankenstein.txt" 
+#book path
 
 def get_book_text(path_to_file):
     with open(path_to_file, "r") as f:
@@ -12,12 +14,34 @@ def main():
     return text 
 #calls the prev function and input the path, return total file contents
 
-result_m  = main() # store main() contents
-book_txt = str(result_m) # convert results to string
+result_m  = main() 
+# store main() contents
+book_txt = str(result_m) 
+# convert results to string
+char_dict = cnt_char(book_txt)
+#store a dict with all chars and num of apperances using cnt_char function from stats.py
+char_list_ordered = dic_to_list(char_dict)
+#list of dicts for each char
 
-
-get_num_words(book_txt)#start the whole prog
-cnt_char(book_txt)
+"""
+print(f
+============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+{get_num_words(book_txt)}
+--------- Character Count -------)#dynamic header for file name
+#start the whole prog
+for i in char_list_ordered:
+    if i['char'].isalpha():
+        print(i['char'],i['num'])
+"""
 
 print("============ BOOKBOT ============")
-print(f"Analyzing book found at {path}...")
+print(f'Analyzing book found at {path}...')
+print("----------- Word Count ----------")
+print(f'Found {get_num_words(book_txt)} total words')
+print("--------- Character Count -------")
+for i in char_list_ordered:
+    if i['char'].isalpha():
+        print(i['char'] + ":" + " " + str(i['num']))
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def get_num_words(book_txt):
     word_ls = book_txt.split()# create list of all the words in a file
     for word in word_ls:#iterate over each word and add 1 to counter for each
         x +=1 
-    print(f'Found {x} total words')#prints total words in file  
+    return x#prints total words in file  
 
 def cnt_char(book_txt):
     book_txt = book_txt.lower()# all text lower case for no dups
@@ -13,4 +13,14 @@ def cnt_char(book_txt):
             char_count[char] = 1# if char not in dic add it   
         else:
             char_count[char] += 1 #if char in dic add 1 for every apearance     
-    print(char_count)
+    return char_count
+
+def dic_to_list(dict):
+    def sort_on(items):
+        return items["num"]
+    characters = []
+    for key, value in dict.items():
+        dict = {"char":key, "num":value}
+        characters.append(dict)
+    characters.sort(reverse=True, key=sort_on)
+    return characters
